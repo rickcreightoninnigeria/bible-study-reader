5cea80a05ff4a0a6afe898223223ccefd5ad398c
Rick Creighton <rick.creighton.in.nigeria@gmail.com>
2026-05-12 17:03:38 +0100

    testing git commit to idb-branch

---
+++ app/js/migrate.js
@@ -0,0 +1,10 @@
+{
+  "appVersion"       :"0.5.12",
+  "estudyVersion"    :"3",
+  "androidPackageId": "com.rickcreighton.bsr",
+  "sampleDownloadId" :"12GWu_cCP8XEyehOY0por--FYaBWloevT",
+  "appCreatedAt"     :"2026-03-28T00:00:00Z",
+  "appUpdatedAt"     :"2026-04-06T00:00:00Z",
+  "appAuthor"        :{ "name":"Rick Creighton" },
+  "appPublisher"     :{ "name":"Rick Creighton" }
+}
